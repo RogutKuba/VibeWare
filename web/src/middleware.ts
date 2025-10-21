588d14c7e8602680f407ec9af11e746086f30036
rogutkuba <rogutkuba@gmail.com>
2025-10-21 14:34:37 -0700

    demo

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,16 +1,28 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
+const USER_TO_DEPLOYMENTS_MAP = {
+  user_34OPUHayKIB0u2jdw8UdCbgT2QL:
+    'vibe-ware-git-vibeware-d4o6dbeiab4hz9rsfb-jnl-rogut-kuba.vercel.app',
+  user_34OQYLsOuLRH85ySWzWBeQRQ4Lr:
+    'vibe-ware-git-vibeware-d4o6dbeiab4hz9rsfb-jnl-rogut-kuba.vercel.app',
+};
+
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   console.log('userId', userId);
 
+  const isUserInMap =
+    USER_TO_DEPLOYMENTS_MAP[userId as keyof typeof USER_TO_DEPLOYMENTS_MAP];
+
   // Only redirect if user is authenticated
-  if (userId) {
+  if (isUserInMap) {
     const url = new URL(req.url);
     const currentHost = url.hostname;
-    const targetHost = `${userId}.vercel.app`;
+    const targetHost = `${
+      USER_TO_DEPLOYMENTS_MAP[userId as keyof typeof USER_TO_DEPLOYMENTS_MAP]
+    }.vercel.app`;
 
     // Only redirect if:
     // 1. We're in production (vercel.app domain)
